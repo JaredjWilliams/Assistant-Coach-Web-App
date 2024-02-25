9f059786d35b17759f69acdac479ffa28042a3d3
Jared Williams <jared.williams101697@gmail.com>
2024-02-25 18:25:41 -0500

    JWT token is authorized upon login

--- src/app/services/temp-auth.service.ts
+++ src/app/services/temp-auth.service.ts
@@ -1,12 +1,31 @@
 import { Injectable } from '@angular/core';
-import { AUTHENTICATED_USER } from '../app.constants';
+import { API_ROOT, AUTHENTICATED_USER, TOKEN } from '../app.constants';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TempAuthService {
 
-  constructor() { }
+  constructor(
+    private http : HttpClient
+  ) { }
+
+  executeJWTAuthenticationService(username : string, password : string) {
+    return this.http.post<any>(`${API_ROOT}/authenticate`, {
+      username,
+      password
+    }).pipe(
+      map(
+        data => {
+          sessionStorage.setItem(AUTHENTICATED_USER, username);
+          sessionStorage.setItem(TOKEN, `Bearer ${data.token}`);
+          return data;
+        }
+      )
+    );
+  }
 
   getAuthenticatedToken() {
     return this.getAuthenticatedUser() != null ? 
